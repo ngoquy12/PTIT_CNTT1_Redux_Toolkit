ba15ae88a61af3c703364453a3b7557d126a3704
ngovanquy <ngoq753429@gmail.com>
2025-09-23 10:19:24 +0700

    update files

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import TaskList from "./components/TaskList";
+
+function App() {
+  return (
+    <>
+      {/* <Counter /> */}
+      <TaskList />
+    </>
+  );
+}
+
+export default App;
